04752ce0ebca6ad4460df8d695680b806041e8fc
anisaaindraa <11211014@student.itk.ac.id>
2023-10-20 23:24:12 +0800

    update history.dart

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,5 +1,3 @@
-// KOSSSSOOONGGGGG
-
 import 'package:flutter/material.dart';
 
 class HistoryPage extends StatefulWidget {
@@ -9,11 +7,51 @@ class HistoryPage extends StatefulWidget {
   _HistoryPageState createState() => _HistoryPageState();
 }
 
-// Define the selected outlet
 class _HistoryPageState extends State<HistoryPage> {
+  // Contoh data riwayat pembelian
+  List<Map<String, dynamic>> purchaseHistory = [
+    {
+      'waktu': '2023-10-18 14:30:00',
+      'diterima': '2023-10-18 15:00:00',
+      'detail': 'Cuci baju',
+      'status': 'Sukses', // Tambahkan status pembelian
+    },
+    {
+      'waktu': '2023-10-17 10:15:00',
+      'diterima': '2023-10-17 11:00:00',
+      'detail': 'Cuci selimut',
+      'status': 'Cancel', // Tambahkan status pembelian
+    },
+    // Tambahkan data riwayat lainnya di sini
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Riwayat Pembelian Laundry'),
+      ),
+      body: ListView.builder(
+        itemCount: purchaseHistory.length,
+        itemBuilder: (context, index) {
+          final history = purchaseHistory[index];
+          final isCancelled = history['status'] == 'Cancel';
+          final textColor = isCancelled ? Colors.red : Colors.green;
+
+          return Card(
+            child: ListTile(
+              title: Text('Waktu: ${history['waktu']}'),
+              subtitle: Text('Diterima: ${history['diterima']}'),
+              trailing: Text(
+                'Detail: ${history['detail']}',
+                style: TextStyle(
+                  color: textColor,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
