9d10b7c02943d05e0a0e18ab11f8275a01c8c1df
carolineadicahya <11211026@student.itk.ac.id>
2023-10-31 14:24:02 +0800

    service laundry

--- lib/service/laundry_service.dart
+++ lib/service/laundry_service.dart
@@ -1,1 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class LaundryService {
+  // final User? user = FirebaseAuth.instance.currentUser;
+  final db = FirebaseFirestore.instance.collection('layanan');
+
+  // READ: list layanan
+  Stream<QuerySnapshot> layanan(String id) {
+    return db
+        .doc(id)
+        .collection('layanan')
+        .orderBy('nama', descending: true)
+        .snapshots();
+  }
+
+  // ADD: tambah layanan
+  Future<void> addPeraturan(Map<String, dynamic> body, String ServiceId) {
+    return db.doc(ServiceId).collection('layanan').add(body);
+  }
+
+  // DELETE: hapus layanan
+  Future<void> deletePeraturan(String ServiceId, String id) {
+    return db.doc(ServiceId).collection('layanan').doc(id).delete();
+  }
+}
